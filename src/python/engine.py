cdcba1c89421ed032025ddc9922f315aba3c6beb
ali_gewei <grey@pku.edu.cn>
2024-11-29 00:33:22 +0800

    finish engine

---
+++ src/python/engine.py
@@ -0,0 +1,22 @@
+class BacktestEngine:
+    def __init__(self, replayer, strategy):
+        self.replayer = replayer
+        self.strategy = strategy
+
+    def run(self):
+        for quote in self.replayer:
+            self.strategy.on_bar(quote)
+
+
+if __name__ == "__main__":
+    import time
+    import datetime as dt
+    from replayer.duck import DuckdbReplayer
+    from bktrader import strategy
+
+    replayer = DuckdbReplayer(start=dt.date(2010, 1, 1), end=dt.date(2024, 11, 30), symbol=510050, uri="bar1d.db")
+    stg = strategy.GridPercent()
+    engine = BacktestEngine(replayer, stg)
+    start_time = time.time()
+    engine.run()
+    print(f"cost {time.time()-start_time:.2f}s")
