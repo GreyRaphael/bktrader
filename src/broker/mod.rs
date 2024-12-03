b3dfdf2bfd722e8163d448d408d74984dd16140c
ali_gewei <grey@pku.edu.cn>
2024-12-03 14:02:28 +0800

    add Analyzer as py module

--- src/broker/mod.rs
+++ src/broker/mod.rs
@@ -5,5 +5,6 @@ pub mod etf;
 pub fn register(parent_module: &Bound<'_, PyModule>) -> PyResult<()> {
     let broker = PyModule::new(parent_module.py(), "broker")?;
     broker.add_class::<etf::EtfBroker>()?;
+    broker.add_class::<analyzer::Analyzer>()?;
     parent_module.add_submodule(&broker)
 }
