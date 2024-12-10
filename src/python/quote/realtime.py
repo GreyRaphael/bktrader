bbd85d4213c94a30cc047609e83feff8d98f36d5
ali_gewei <grey@pku.edu.cn>
2024-12-10 22:55:25 +0800

    finish two quoter

--- src/python/quote/realtime.py
+++ src/python/quote/realtime.py
@@ -26,11 +26,7 @@ def predicted_close_ratio(update_time: dt.datetime):
 class EastQuote:
     def __init__(self, uri: str):
         self.uri = uri
-        self.client = httpx.Client(
-            headers={
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0",
-            }
-        )
+        self.client = httpx.Client(headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0"})
         self.mapping = {
             "f12": "code",
             "f297": "dt",
@@ -44,6 +40,7 @@ class EastQuote:
             "f441": "iopv",
             "f124": "update_time",
         }
+        self.latest_bars = {}
 
     def days_since_epoch(self, value: int) -> int:
         year = value // 10000  # 2024
@@ -53,8 +50,6 @@ class EastQuote:
         return delta.days
 
     def update(self):
-        fields = ",".join(self.mapping.keys())
-        timestamp = int(dt.datetime.now().timestamp() * 1000)
         # MK0021: A
         # MK0022: currency
         # MK0023: foreign
@@ -72,35 +67,20 @@ class EastQuote:
             "wbp2u": "|0|0|0|web",
             "fid": "f5",  # sort by volume
             "fs": "b:MK0023",  # market type: b:MK0021,b:MK0022,b:MK0023,b:MK0024,b:MK0827
-            "fields": fields,
-            "_": timestamp,
+            "fields": ",".join(self.mapping.keys()),
+            "_": int(dt.datetime.now().timestamp() * 1000),  # timestamp
         }
-        rsp = self.client.get("http://push2.eastmoney.com/api/qt/clist/get", params=url_params, timeout=10).json()
+        rsp = self.client.get("http://push2.eastmoney.com/api/qt/clist/get", params=url_params, timeout=5).json()
 
-        bar_dict = {}
         for record in rsp["data"]["diff"]:
             update_time = dt.datetime.fromtimestamp(record["f124"])  # run after 14:00
-
-            morning_start = dt.datetime.combine(update_time.date(), dt.time(9, 30, 0))
-            morning_end = dt.datetime.combine(update_time.date(), dt.time(11, 30, 0))
-            afternoon_start = dt.datetime.combine(update_time.date(), dt.time(13, 0, 0))
-            afternoon_end = dt.datetime.combine(update_time.date(), dt.time(15, 0, 0))
-            if update_time < morning_start:
-                return {}
-            elif update_time <= morning_end:
-                time_ratio = (update_time - morning_start) / dt.timedelta(hours=4)
-            elif update_time < afternoon_start:
-                time_ratio = 0.5
-            elif update_time <= afternoon_end:
-                time_ratio = (update_time - afternoon_start + dt.timedelta(hours=2)) / dt.timedelta(hours=4)
-            else:
-                time_ratio = 1.0
+            time_ratio = predicted_close_ratio(update_time)
 
             predicted_today_volume = record["f5"] * 1e2 / time_ratio  # 1e2单位手换算成股
             predicted_today_amount = record["f6"] / time_ratio
 
             code = int(record["f12"])
-            bar_dict[code] = (
+            self.latest_bars[code] = (
                 code,  # code
                 self.days_since_epoch(record["f297"]),  # dt
                 record["f18"],  # preclose
@@ -112,14 +92,12 @@ class EastQuote:
                 predicted_today_amount,  # amount
             )
 
-        self.realtime_bars = bar_dict
-
     def get_quote(self, code: int) -> datatype.Bar:
         with duckdb.connect(self.uri, read_only=True) as conn:
             query = "SELECT ROUND(close/1e4, 3), adjfactor FROM etf WHERE code=? ORDER BY dt DESC LIMIT 1"
-            last_close, factor = conn.execute(query, [code]).fetchone()
-        code, dt, preclose, open, high, low, last, predicted_vol, predicted_amt = self.realtime_bars[code]
-        adjfactor = factor if math.isclose(last_close, preclose) else last_close / preclose * factor
+            duck_last_close, factor = conn.execute(query, [code]).fetchone()
+        code, dt, preclose, open, high, low, last, predicted_vol, predicted_amt = self.latest_bars[code]
+        adjfactor = factor if math.isclose(duck_last_close, preclose) else duck_last_close / preclose * factor
         # print("prev", factor, "now", adjfactor)
         return datatype.Bar(
             code=code,
@@ -166,7 +144,7 @@ class XueQiuQuote:
         time_ratio = predicted_close_ratio(update_dt)
         preclose = quote["last_close"]
         adjfactor = factor if math.isclose(duck_last_close, preclose) else duck_last_close / preclose * factor
-        # print("adjfactor", adjfactor)
+        # print("prev", factor, "now", adjfactor)
         return datatype.Bar(
             code=int(quote["code"]),
             dt=(update_dt.date() - dt.date(1970, 1, 1)).days,
@@ -181,11 +159,11 @@ class XueQiuQuote:
 
 
 if __name__ == "__main__":
-    # east_bar = EastQuote(uri="bar1d.db")
-    # east_bar.update()
-    # print(east_bar.get_quote(513650))
-    # print(east_bar.get_quote(159659))
+    east_quoter = EastQuote(uri="bar1d.db")
+    east_quoter.update()
+    print(east_quoter.get_quote(513650))
+    print(east_quoter.get_quote(159659))
 
-    xueqiu_bar = XueQiuQuote(uri="bar1d.db")
-    bar = xueqiu_bar.get_quote(513650)
-    print(bar)
+    xueqiu_quoter = XueQiuQuote(uri="bar1d.db")
+    print(xueqiu_quoter.get_quote(513650))
+    print(xueqiu_quoter.get_quote(159659))
