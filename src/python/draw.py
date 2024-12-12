464e7da8e5306a46816fa9d180444223a87bfa0d
BeGeWei <grey@pku.edu.cn>
2024-12-13 01:50:35 +0800

    add draw_candles

--- src/python/draw.py
+++ src/python/draw.py
@@ -42,3 +42,82 @@ def draw_ls_chart(positions: list):
             legend_opts=opts.LegendOpts(is_show=False),
         )
     )
+
+
+def draw_candles(chart_data):
+    kline_data = [data[1:-1] for data in chart_data["values"]]
+    kline = (
+        Kline()
+        .add_xaxis(xaxis_data=chart_data["categoryData"])
+        .add_yaxis(
+            series_name="510050",
+            y_axis=kline_data,
+            itemstyle_opts=opts.ItemStyleOpts(
+                color="#ef232a",
+                color0="#14b143",
+                border_color="#8A0000",
+                border_color0="#008F28",
+            ),
+        )
+        .set_global_opts(
+            legend_opts=opts.LegendOpts(is_show=True),
+            datazoom_opts=[
+                opts.DataZoomOpts(
+                    type_="inside",
+                    xaxis_index=[0, 1],
+                    range_start=95,
+                    range_end=100,
+                ),
+                opts.DataZoomOpts(
+                    type_="slider",
+                    xaxis_index=[0, 1],
+                    range_start=95,
+                    range_end=100,
+                ),
+            ],
+            visualmap_opts=opts.VisualMapOpts(
+                is_show=False,
+                series_index=1,
+                is_piecewise=True,
+                pieces=[
+                    {"value": 1, "color": "#ef232a"},
+                    {"value": -1, "color": "#14b143"},
+                ],
+            ),
+            tooltip_opts=opts.TooltipOpts(trigger="axis", axis_pointer_type="cross"),
+            axispointer_opts=opts.AxisPointerOpts(
+                is_show=True,
+                link=[{"xAxisIndex": "all"}],
+            ),
+        )
+    )
+
+    bar = (
+        Bar()
+        .add_xaxis(xaxis_data=chart_data["categoryData"])
+        .add_yaxis(
+            series_name="Volume",
+            y_axis=chart_data["volumes"],
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+        .set_global_opts(
+            xaxis_opts=opts.AxisOpts(
+                axistick_opts=opts.AxisTickOpts(is_show=False),
+                axislabel_opts=opts.LabelOpts(is_show=False),
+                splitline_opts=opts.SplitLineOpts(is_show=False),
+            ),
+            yaxis_opts=opts.AxisOpts(
+                is_scale=True,
+                axislabel_opts=opts.LabelOpts(is_show=False),
+                splitline_opts=opts.SplitLineOpts(is_show=False),
+            ),
+            legend_opts=opts.LegendOpts(is_show=False),
+        )
+    )
+
+    # kline + Bar
+    grid_chart = Grid(init_opts=opts.InitOpts(width="100%", height="700px", theme="vintage"))
+    grid_chart.add(kline, grid_opts=opts.GridOpts(pos_left=50, pos_top=20, pos_right=20, height="70%"))
+    grid_chart.add(bar, grid_opts=opts.GridOpts(pos_left=50, pos_top="75%", pos_right=20, height="15%"))
+
+    return grid_chart
