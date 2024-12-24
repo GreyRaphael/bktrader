4f476b6f4fe6fccb6d0ef9a607eeac738a6d208c
ali_gewei <grey@pku.edu.cn>
2024-12-24 21:58:42 +0800

    add all situation

--- src/python/quote/fundtype.py
+++ src/python/quote/fundtype.py
@@ -5,9 +5,11 @@ class ETFType(str, enum.Enum):
     qdii = "qdii"
     commodity = "commodity"
     bond = "bond"
+    all = "all"
 
 
 class LOFType(str, enum.Enum):
     qdii = "qdii"
     commodity = "commodity"  # alternative investment
     bond = "bond"
+    all = "all"
