6430d81b4f0df86d2f4746703512570a705685eb
KTC9_gewei <grey@pku.edu.cn>
2024-12-30 15:36:13 +0800

    add struct `BarM`

--- src/datatype/mod.rs
+++ src/datatype/mod.rs
@@ -5,6 +5,7 @@ pub mod quote;
 pub fn register(parent_module: &Bound<'_, PyModule>) -> PyResult<()> {
     let datatype = PyModule::new(parent_module.py(), "datatype")?;
     datatype.add_class::<quote::Bar>()?;
+    datatype.add_class::<quote::BarM>()?;
     datatype.add_class::<position::Position>()?;
     parent_module.add_submodule(&datatype)
 }
--- src/datatype/quote.rs
+++ src/datatype/quote.rs
@@ -55,6 +55,55 @@ impl Bar {
     }
 }
 
+#[pyclass]
+#[derive(Debug)]
+pub struct BarM {
+    #[pyo3(get)]
+    pub code: u32,
+    #[pyo3(get)]
+    pub dt: i64,
+    #[pyo3(get)]
+    pub preclose: f64,
+    #[pyo3(get)]
+    pub open: f64,
+    #[pyo3(get)]
+    pub high: f64,
+    #[pyo3(get)]
+    pub low: f64,
+    #[pyo3(get)]
+    pub close: f64,
+    #[pyo3(get)]
+    pub volume: f64,
+    #[pyo3(get)]
+    pub amount: f64,
+    #[pyo3(get)]
+    pub trades_count: f64,
+}
+
+#[pymethods]
+impl BarM {
+    #[new]
+    #[pyo3(signature = (code=0, dt=0, preclose=0.0, open=0.0, high=0.0, low=0.0, close=0.0,  volume=0.0, amount=0.0, trades_count=0.0))]
+    fn new(code: u32, dt: i64, preclose: f64, open: f64, high: f64, low: f64, close: f64, volume: f64, amount: f64, trades_count: f64) -> Self {
+        Self {
+            code,
+            dt,
+            preclose,
+            open,
+            high,
+            low,
+            close,
+            volume,
+            amount,
+            trades_count,
+        }
+    }
+
+    fn __repr__(&self) -> String {
+        format!("{:?}", self)
+    }
+}
+
 // Tick
 #[allow(dead_code)]
 pub struct Tick {
