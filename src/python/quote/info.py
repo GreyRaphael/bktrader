1f039e63a84fae3c3dd7ba98f5cd8982f21707dc
BeC9_gewei <grey@pku.edu.cn>
2025-01-03 23:33:11 +0800

    fix code exist in info not in bar1d

--- src/python/quote/info.py
+++ src/python/quote/info.py
@@ -10,5 +10,5 @@ def query_info_all(uri: str) -> dict:
 def query_sector_codes(uri: str, sectors: list[int]) -> list[int]:
     placeholders = ",".join([str(s) for s in sectors])
     with duckdb.connect(uri, read_only=True) as conn:
-        records = conn.execute(f"SELECT code FROM info WHERE sector IN ({placeholders})").fetchall()
+        records = conn.execute(f"SELECT info.code FROM info JOIN bar1d ON info.code=bar1d.code WHERE info.sector IN ({placeholders}) AND bar1d.dt = (SELECT MAX(bar1d.dt) FROM bar1d)").fetchall()
     return [record[0] for record in records]
