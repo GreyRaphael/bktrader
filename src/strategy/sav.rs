65079d64d6eeb16a0fb484d8c08e996f45723a92
KTC9_gewei <grey@pku.edu.cn>
2025-01-08 11:21:07 +0800

    add problem comments

--- src/strategy/sav.rs
+++ src/strategy/sav.rs
@@ -13,6 +13,7 @@ pub struct SavStg {
 
 impl QuoteHandler<BarM> for SavStg {
     fn on_quote(&mut self, quote: &BarM) {
+        // self.broker.update_portfolio_value(quote); # quote is not type Bar
         todo!()
     }
 }
@@ -21,8 +22,16 @@ impl QuoteHandler<BarM> for SavStg {
 impl SavStg {
     #[new]
     #[pyo3(signature = (init_cash=5e5, max_active_pos_len=6))]
-    pub fn new(init_cash: f64,max_active_pos_len: usize)->Self {
+    pub fn new(init_cash: f64, max_active_pos_len: usize) -> Self {
         let origin_amount = init_cash / max_active_pos_len as f64;
-        Self { broker: EtfBroker::new(init_cash, 5.0, 1.5e-4), entry_amount: origin_amount, available_pos_num: max_active_pos_len, }
+        Self {
+            broker: EtfBroker::new(init_cash, 5.0, 1.5e-4),
+            entry_amount: origin_amount,
+            available_pos_num: max_active_pos_len,
+        }
+    }
+
+    pub fn on_update(&mut self, quote: &BarM) {
+        self.on_quote(quote);
     }
 }
